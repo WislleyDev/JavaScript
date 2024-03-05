d7a55ccdfefb60090dde63b0d2779e02df62d10c
Wislley marques vieira dos santos <w52993000@gmail.com>
2024-03-05 17:15:16 -0300

    fiz 2 desafios

---
+++ desafios/ex002/modelo hora/script.js
@@ -0,0 +1,24 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = 18//data.getHours()
+    // hora = 11
+
+    msg.innerHTML = `<p>Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        //* BOM DIA!
+        img.src = 'img/manha.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        //* BOA TARDE!
+        img.src = 'img/tarde.jpg'
+        document.body.style.background = '#b9846f'
+
+    } else if (hora >= 18){
+        //* BOA NOITE!
+        img.src = 'img/noite.jpg'
+        document.body.style.background = '#515154'
+    }
+}
